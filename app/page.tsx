b8c6700605a0b3d56dcbea6d550016fdeeb4c98c
Ami <amiweil2@gmail.com>
2026-02-20 01:12:37 +0200

    refactor: simplify page entry and remove unused animation dep

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,27 @@
-import App from "./App";
+"use client";
+
+import { Header } from "./components/Header";
+import { HeroSection } from "./components/HeroSection";
+import { PainSection } from "./components/PainSection";
+import { HowItWorksSection } from "./components/HowItWorksSection";
+import { TrustSection } from "./components/TrustSection";
+import { FAQSection } from "./components/FAQSection";
+import { WaitlistForm } from "./components/WaitlistForm";
+import { Footer } from "./components/Footer";
 
 export default function Page() {
-  return <App />;
+  return (
+    <div className="size-full" dir="rtl">
+      <Header />
+      <main>
+        <HeroSection />
+        <PainSection />
+        <HowItWorksSection />
+        <TrustSection />
+        <FAQSection />
+        <WaitlistForm />
+      </main>
+      <Footer />
+    </div>
+  );
 }
